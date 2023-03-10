5c55203a9b09f5ff6d5f009558964ea8db74eb49
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-03-10 13:26:00 -0300

    Atualização no visual e na lógica

--- js/script.js
+++ js/script.js
@@ -9,14 +9,20 @@ function calcularLucro() {
         , valor_produto = Number(valor_pdt.value)
         , mult_qtd_vlr = (qtd_produto * valor_produto)
         , perda_producao = (mult_qtd_vlr * 0.28)
-        , perda_manejo = (mult_qtd_vlr * 0.22)
-        , perda_mercado = (mult_qtd_vlr * 0.17)
+        , perda_manejo = ((mult_qtd_vlr - perda_producao) * 0.22)
+        , perda_mercado = ((mult_qtd_vlr - perda_manejo) * 0.17)
         , ganho_atual = (mult_qtd_vlr - (perda_producao + perda_manejo + perda_mercado))
         , ganho_futuro = (mult_qtd_vlr + ganho_atual)
         , lucro = Math.round(((ganho_futuro - ganho_atual) / ganho_futuro) * 100);
 
     div_resposta.innerHTML = `Estimamos um lucro aproximado de <span style="color: green; font-weight: bold;">R$${Math.round(ganho_atual)} reais</span> por mês com nosso sistema!`
-    div_retorno.innerHTML += `${mult_qtd_vlr} para <span style="color: green;">${ganho_futuro}</span> (<span style="color: green;">${lucro}%!</span>) <br> <br>`
+
+    div_retorno.innerHTML = `No mercado de laticínios, é comum a perda de uma certa contia do seu lucro devido problemas internos e externos. <br>
+    A perda de laticínios por produção de lote, é de <span style="color: red; font-weight: bold;">28%</span>, de manejo <span style="color: red; font-weight: bold;">22%</span> e mercado <span style="color: red; font-weight: bold;">17%</span> <br>
+    Sabendo que a perda por produção, manejo e mercado em sua situação são, respectivamente, <span style="color: red; font-weight: bold;">${Math.round(perda_producao)}%</span>, <span style="color: red; font-weight: bold;">${Math.round(perda_manejo)}%</span> e <span style="color: red; font-weight: bold;">${Math.round(perda_mercado)}%</span>, seu lucro atual é de <span style="text-decoration: underline green;">R$${mult_qtd_vlr}</span> reais por mês. <br> <br>
+    Observe o seu lucro mensal: <br> <br>`
+    
+    div_retorno.innerHTML += `${mult_qtd_vlr} para aproximadamente <span style="color: green;">${Math.round(ganho_futuro)}</span> (<span style="color: green;">${lucro}%!</span>) <br>`
 }
 
 btn_submit.addEventListener('click', calcularLucro);
